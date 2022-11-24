6110f7ce99b573bc56bba53450e31521cd1a3fb0
shah <workwithme.shah@gmail.com>
2022-11-24 22:51:40 +0530

    operators

--- 1. Fundamentals/script.js
+++ 1. Fundamentals/script.js
@@ -1,9 +1,9 @@
+//-------------------------------------------------------------------
 // Values and variables
-
 // let firstName = "Shah"; // firstName => Variable, "Shah" => value
 // console.log(firstName);
 
-
+//-------------------------------------------------------------------
 // Data types
 // let firstName = "Shah";
 // let age = 22;
@@ -26,4 +26,30 @@
 // console.log(birthDate);
 // birthDate = 25; // error => cannot reassign to const variable
 
+//-------------------------------------------------------------------
+//Operator
+const now = 2000;
+const numb1 = now + 25;
+const numb2 = now - 25;
+const numb3 = now * 25;
+const numb4 = now / 25;
+console.log(numb1, numb2, numb3, numb4, 2 ** 3); // ** means 2 power 3
 
+
+const firstName = "Shah";
+const lastName = "Raza";
+
+console.log(firstName + " " + lastName); // string concat
+// Assignment operator
+// let x = 10;
+// x = 10 + 5;
+// x += 15;  //x = x + 15;
+// x++; // x = x + 1;
+// x--; // x = x - 1;
+
+//Comparision operator
+// let a = 10;
+// let b = 20;
+
+// console.log(a > b); //false
+// console.log(a >= 10); // true
