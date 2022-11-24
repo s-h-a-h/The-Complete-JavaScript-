d52a8d0de8b00f7b242369976d3bc709de6b7961
shah <workwithme.shah@gmail.com>
2022-11-24 22:32:40 +0530

    data types

--- 1. Fundamentals/script.js
+++ 1. Fundamentals/script.js
@@ -1,4 +1,19 @@
 // Values and variables
 
-let firstName = "Shah"; // firstName => Variable, "Shah" => value
-console.log(firstName);
+// let firstName = "Shah"; // firstName => Variable, "Shah" => value
+// console.log(firstName);
+
+
+// Data types
+// let firstName = "Shah";
+// let age = 22;
+// let isEmployed = true;
+// let department;
+
+// console.log(typeof firstName);
+// console.log(typeof age);
+// console.log(typeof isEmployed);
+// console.log(typeof department);
+
+
+
